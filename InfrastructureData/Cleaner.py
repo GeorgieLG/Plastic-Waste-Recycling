b0f02c77501502001e87d735c44ec3e8a2ce91fc
GeorgieLG <mgeorger04@gmail.com>
2025-06-04 12:57:33 -0400

    Added States to CSV

---
+++ InfrastructureData/Cleaner.py
@@ -0,0 +1,35 @@
+import csv
+
+#Simple script to remove duplicate rows from a CSV file
+def removeDuplicateRows(input_file, output_file):
+    seen = set()
+    with open(input_file, 'r', newline='') as infile:
+        with open(output_file, 'w', newline='') as outfile:
+            reader = csv.reader(infile)
+            writer = csv.writer(outfile)
+            
+            for row in reader:
+                row_tuple = tuple(row)  # Convert list to tuple for immutability
+                if row_tuple not in seen:
+                    seen.add(row_tuple)
+                    writer.writerow(row)
+
+#Parses address data and adds a column for the state
+def getState(input_file, output_file):
+    with open(input_file, 'r', newline='') as infile:
+        with open(output_file, 'w', newline='') as outfile:
+            reader = csv.DictReader(infile)
+            fieldnames = reader.fieldnames + ['State']
+            writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+            writer.writeheader()
+            
+            for row in reader:
+                address = row.get('Address', '')
+                addressList = address.split(',')
+                length = len(addressList)
+                state = addressList[length - 2]
+                row['State'] = state.strip()
+                writer.writerow(row)
+
+#removeDuplicateRows('InfrastructureData/InfrastructureData.csv', 'InfrastructureData/InfrastructureData_cleaned.csv')
+getState('InfrastructureData/InfrastructureData.csv', 'InfrastructureData/InfrastructureData_cleaned.csv')
---
+++ InfrastructureData/Graphs.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv('InfrastructureData/InfrastructureData_cleaned.csv')
+
+x = data['']
