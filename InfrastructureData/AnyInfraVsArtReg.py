bc767217bc380a201f25cbb7ee4a4db89801090f
GeorgieLG <mgeorger04@gmail.com>
2025-06-24 12:08:22 -0400

    Fixed

---
+++ InfrastructureData/AnyInfraVsArtReg.py
@@ -0,0 +1,48 @@
+import csv
+
+def csvtodict(filename):
+    statecount = {}
+    with open(filename, 'r', newline='') as infile:
+        reader = csv.reader(infile)
+        header = next(reader)
+        for row in reader:
+            state = row[11]
+            if state in statecount:
+                statecount[state] += 1
+            else:
+                statecount[state] = 1
+
+    with open('InfrastructureData/CSVData/2020PopulationData.csv', 'r', newline='') as popfile:
+        popreader = csv.reader(popfile)
+        next(popreader)
+        for row in popreader:
+            state = row[4]
+            if state not in statecount:
+                statecount[state] = 0
+    return statecount
+
+def getStateReg(filename):
+    stateReg = {}
+    with open(filename, 'r', newline='') as infile:
+        reader = csv.reader(infile)
+        header = next(reader)
+        for row in reader:
+            state = row[0]
+            reg = row[2]
+            stateReg[state] = reg
+    stateRegSorted = {k: v for k, v in sorted(stateReg.items(), key=lambda x: x[0])}
+    return stateRegSorted
+
+def getRegInfraCounts():
+    stateReg = getStateReg('InfrastructureData/CSVData/StateARTreg.csv')
+    stateInfraCounts = csvtodict('InfrastructureData/CSVData/InfrastructureData_cleaned.csv')
+    regInfraCounts = {}
+    for state in stateReg:
+        reg = stateReg[state]
+        if reg not in regInfraCounts:
+            regInfraCounts[reg] = 0
+        else:
+            regInfraCounts[reg] += stateInfraCounts[state]
+    return regInfraCounts
+
+print(getRegInfraCounts())
